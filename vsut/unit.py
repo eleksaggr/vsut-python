4ddef19236c763b4540235404e8a0af1308237b2
Alex Egger <alex.egger96@gmail.com>
2016-01-31 01:15:34 +0100

    Add different asserts and a fail, refine test runner

--- vsut/unit.py
+++ vsut/unit.py
@@ -16,43 +16,149 @@ class Suite:
         if case is not None and isinstance(case, Case):
             self.cases.append(case)
 
-    def run(self, out=stdout):
-        results = []
+    def run(self, out=stdout, verbose=False):
         id = 0
         for case in self.cases:
-            result = None
             try:
-                case.run()
-                result = Result(id, Status('Ok'), "\t\t")
-            except AssertionError as e:
-                result = Result(id, Status('Fail'), e)
+                # TODO: Run all methods of the case that start with 'test'.
+                print(case.tests)
+                for test in case.tests:
+                    func = getattr(case, test, None)
+                    func()
             except FailError as e:
-                # Should we run into a fail condition exit the loop.
-                result = Result(id, Status('Error'), e)
-                results.append(result)
-                break
-
-            results.append(result)
+                # If we hit a FailError, stop execution and skip to the next
+                # case.
+                pass
             id = id + 1
 
         print("Suite: {0}".format(self.name), file=out)
-        for result in results:
-            print("{0}: {1} ({2})".format(
-                result.id, result.message, result.status), file=out)
+        print("***************************************************************")
+        print("\t--------------------------------------------------")
+        for case in self.cases:
+            success = True
+
+            print("\tCase: {0}".format(case.name), file=out)
+            for result in case.results:
+                # Convert enum status to string representation.
+                if result.status == Status.Ok:
+                    status = "OK"
+                elif result.status == Status.Fail:
+                    status = "FAIL"
+                    success = False
+                else:
+                    status = "ERROR"
+                    success = False
+                if verbose:
+                    # If there is a message, add parentheses to it.
+                    if result.message is not None and result.message is not "":
+                        result = Result(result.id, result.status,
+                                        "({0})".format(result.message))
+
+                    print("\t\t{0}:\t {2} {1}".format(
+                        result.id, result.message, status), file=out)
+                    print("\t----------------------------------------------------")
+            if not verbose:
+                print("\t\tSuccess: {0}".format(success))
+                print("\t----------------------------------------------------")
+        print("***************************************************************")
+
+
+def test(func):
+    def func_wrapper(self):
+        if func.__name__ not in self.tests:
+            self.tests.append(func.__name__)
+    return func_wrapper
 
 
 class Case:
 
-    def run(self):
-        pass
+    def __init__(self, name):
+        self.name = name
+        self.id = 0
+        self.results = []
+        self.tests = []
 
-    def assertEqual(value, expected):
-        if value != expected:
-            raise AssertionError("{0} is not {1}".format(value, expected))
+    def run(self, method):
+        self.method()
 
-    def failUnless(value, expected):
+    def assertEqual(self, value, expected):
+        if value == expected:
+            self.results.append(Result(self.id, Status.Ok, ""))
+        else:
+            self.results.append(Result(self.id, Status.Fail,
+                                       "{0} is not {1}".format(value, expected)))
+        self.id = self.id + 1
+
+    def assertNotEqual(self, value, expected):
         if value != expected:
-            raise FailError("{0} is not {1}".format(value, expected))
+            self.results.append(Result(self.id, Status.Ok, ""))
+        else:
+            self.results.append(Result(self.id, Status.Fail,
+                                       "{0} is not {1}".format(value, expected)))
+        self.id = self.id + 1
+
+    def assertTrue(self, value):
+        self.assertEqual(value, True)
+
+    def assertFalse(self, value):
+        self.assertEqual(value, False)
+
+    def assertIs(self, value, expected):
+        if value is expected:
+            self.results.append(Result(self.id, Status.Ok, ""))
+        else:
+            self.results.append(Result(self.id, Status.Fail,
+                                       "{0} is not {1}".format(value, expected)))
+        self.id = self.id + 1
+
+    def assertIsNot(self, value, expected):
+        if value is not expected:
+            self.results.append(Result(self.id, Status.Ok, ""))
+        else:
+            self.results.append(Result(self.id, Status.Fail,
+                                       "{0} is {1}".format(value, expected)))
+        self.id = self.id + 1
+
+    def assertIsNone(self, value):
+        self.assertIs(value, None)
+
+    def assertIsNotNone(self, value):
+        self.assertIsNot(value, None)
+
+    def assertIn(self, value, collection):
+        if value in collection:
+            self.results.append(Result(self.id, Status.Ok, ""))
+        else:
+            self.results.append(
+                Result(self.id, Status.Fail, "{0} is not in {1}".format(value, collection)))
+        self.id = self.id + 1
+
+    def assertNotIn(self, value, collection):
+        if value not in collection:
+            self.results.append(Result(self.id, Status.Ok, ""))
+        else:
+            self.results.append(
+                Result(self.id, Status.Fail, "{0} is in {1}".format(value, collection)))
+        self.id = self.id + 1
+
+    def assertRaises(self, exception, func, *args, **kwargs):
+        try:
+            func(*args, **kwargs)
+            self.results.append(Result(
+                self.id, Status.Fail, "{0} did not throw {1}".format(func.__name__, exception)))
+        except exception as e:
+            self.results.append(Result(self.id, Status.Ok, ""))
+        self.id = self.id + 1
+
+    def failUnless(self, value, expected):
+        if value == expected:
+            self.results.append(Result(self.id, Status.Ok, ""))
+        else:
+            self.results.append(
+                Result(self.id, Status.Error, "{0} is not {1} | Execution stopped.".format(value, expected)))
+            raise FailError("")
+        self.id = self.id + 1
+
 
 class FailError(Exception):
 
